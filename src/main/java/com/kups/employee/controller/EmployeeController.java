73cbc6892d77776a3ea14a06ff7dea2d8fd785c5
8917239419 <upasanajava9990@gmail.com>
2022-06-29 02:12:37 +0530

    first commit

---
+++ src/main/java/com/kups/employee/controller/EmployeeController.java
@@ -0,0 +1,28 @@
+package com.kups.employee.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kups.employee.model.Employee;
+import com.kups.employee.repository.EmployeeRepo;
+
+
+
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+	
+	@Autowired
+	private EmployeeRepo employeeRepo;
+	
+	//get all employee
+	@GetMapping("/employees")
+	public List<Employee> getAllEmployees(){
+		return employeeRepo.findAll();
+	}
+
+}
---
+++ src/main/java/com/kups/employee/repository/EmployeeRepo.java
@@ -0,0 +1,10 @@
+package com.kups.employee.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.kups.employee.model.Employee;
+@Repository
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+
+}
