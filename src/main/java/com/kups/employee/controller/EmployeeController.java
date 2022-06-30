20c642276745acdcdbef1aad9e6f2cea44bcadfb
8917239419 <upasanajava9990@gmail.com>
2022-07-01 02:40:41 +0530

    logger added
    Signed-off-by: 8917239419 <upasanajava9990@gmail.com>

--- src/main/java/com/kups/employee/controller/EmployeeController.java
+++ src/main/java/com/kups/employee/controller/EmployeeController.java
@@ -2,6 +2,8 @@ package com.kups.employee.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,14 +17,19 @@ import com.kups.employee.repository.EmployeeRepo;
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
+	private static final Logger logger = LoggerFactory
+			.getLogger(EmployeeController.class.getName());
 	//Autowired the dependent class
 	@Autowired
 	private EmployeeRepo employeeRepo;
 	
 	//get all employee
 	@GetMapping("/employees")
-	public List<Employee> getAllEmployees(){
-		return employeeRepo.findAll();
+	public void getAllEmployees(){
+		logger.info("method entry into getAllEmployees()");
+		throw new NullPointerException();
+		//return employeeRepo.findAll();
 	}
+	
 
 }
